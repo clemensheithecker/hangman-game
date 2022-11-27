35bf1391d4f7c50537c6a833bfc5b408a6958be1
Clemens S. Heithecker <clemens@heithecker.dev>
2022-11-27 13:12:23 +0100

    Clean up JavaScript code

--- index.js
+++ index.js
@@ -18,46 +18,38 @@ const SECRET_WORDS = [
   "pants",
   "dishwasher",
 ];
+const SECRET_WORD = SECRET_WORDS[getRandomInt(SECRET_WORDS.length)];
+const SECRET_WORD_LETTERS = SECRET_WORD.split("");
+const SECRET_WORD_LETTERS_UNIQUE = [...new Set(SECRET_WORD_LETTERS)].sort();
+
+const ELEMENT_COUNTDOWN_TEXT = document.getElementById("countdown-text");
+const ELEMENT_LETTER = document.querySelectorAll(".letter");
+const ELEMENT_RETRY_BUTTON = document.getElementById("retry-button");
+const ELEMENT_SECRET_WORD = document.getElementById("secret-word");
 
 let countdown = 10;
-let correctLetters = [];
-
-const SECRET_WORD = SECRET_WORDS[getRandomInt(SECRET_WORDS.length)];
-console.log(SECRET_WORD);
-const SECRET_WORD_LETTERS = SECRET_WORD.split("");
-const SECRET_WORD_UNIQUE_LETTERS = [...new Set(SECRET_WORD_LETTERS)].sort();
-
-const SECRET_WORD_WRAPPER = document.getElementById("secret-word");
-const LETTER_BUTTONS = document.querySelectorAll(".letter");
-
-let ELEMENT_COUNTDOWN = document.getElementById("countdown");
+let guessedCorrectLetters = [];
 
 SECRET_WORD_LETTERS.forEach((letter) => {
-  SECRET_WORD_WRAPPER.innerHTML += `<div class="secret-letter-wrapper">\n<span class="secret-letter secret-letter-hidden">${letter.toUpperCase()}</span>\n</div>`;
+  ELEMENT_SECRET_WORD.innerHTML += `<div class="secret-letter-wrapper">\n<span class="secret-letter secret-letter-hidden">${letter.toUpperCase()}</span>\n</div>`;
 });
 
 listenToKeyboard();
 
-LETTER_BUTTONS.forEach((button) => {
+ELEMENT_LETTER.forEach((button) => {
   button.addEventListener("click", (event) => {
-    const CLICKED_LETTER = event.target.innerHTML.toLowerCase();
+    const GUESSED_LETTER = event.target.innerHTML.toLowerCase();
 
     button.disabled = true;
 
-    if (isLetterInSecretWord(CLICKED_LETTER)) {
-      console.log(CLICKED_LETTER);
+    if (isLetterInSecretWord(GUESSED_LETTER)) {
       button.classList.add("correct-letter");
+      guessedCorrectLetters.push(GUESSED_LETTER);
+      displayGuessedCorrectLetter(GUESSED_LETTER);
 
-      correctLetters.push(CLICKED_LETTER);
-      displayGuessedCorrectLetter(CLICKED_LETTER);
-
-      if (isWinner(SECRET_WORD_UNIQUE_LETTERS, correctLetters)) {
-        console.log("we have a winner!!");
-
-        setTimeout(() => {
-          JS_CONFETTI.addConfetti();
-          displayWinnerMessage();
-        }, 500);
+      if (isWinner(SECRET_WORD_LETTERS_UNIQUE, guessedCorrectLetters)) {
+        ELEMENT_LETTER.forEach((button) => (button.disabled = true));
+        displayWinnerMessage();
       }
     } else {
       button.classList.add("wrong-letter");
@@ -65,58 +57,56 @@ LETTER_BUTTONS.forEach((button) => {
         countdown -= 1;
 
         if (countdown === 0) {
-          LETTER_BUTTONS.forEach((b) => {
+          ELEMENT_LETTER.forEach((b) => {
             b.disabled = true;
           });
         }
       } else {
         countdown = 0;
       }
-      ELEMENT_COUNTDOWN.innerHTML = `${countdown} Tries Left`;
+      ELEMENT_COUNTDOWN_TEXT.innerHTML = `${countdown} Tries Left`;
     }
   });
 });
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+ELEMENT_RETRY_BUTTON.addEventListener("click", () => {
+  location.reload();
+});
+
+function displayGuessedCorrectLetter(guessedLetter) {
+  const ELEMENT_SECRET_LETTER = document.querySelectorAll(".secret-letter");
+
+  ELEMENT_SECRET_LETTER.forEach((letter) => {
+    if (letter.innerHTML.toLowerCase() === guessedLetter) {
+      letter.classList.remove("secret-letter-hidden");
+    }
+  });
+}
+
+function displayWinnerMessage() {
+  const ELEMENT_ALPHABET = document.querySelector(".alphabet");
+  const ELEMENT_COUNTDOWN = document.querySelector(".countdown");
+  const ELEMENT_WINNER_MESSAGE = document.querySelector(".winner-message");
+
+  ELEMENT_ALPHABET.classList.add("display-none");
+  ELEMENT_COUNTDOWN.classList.add("countdown-winner");
+  ELEMENT_WINNER_MESSAGE.classList.remove("display-none");
+
+  JS_CONFETTI.addConfetti();
+}
+
+function getRandomInt(upperBound) {
+  return Math.floor(Math.random() * upperBound);
 }
 
 function isLetterInSecretWord(letter) {
   return SECRET_WORD_LETTERS.includes(letter);
 }
 
-function isWinner(secretWordLetters, correctLetters) {
-  console.log(secretWordLetters, correctLetters.sort());
-  if (secretWordLetters.join() === correctLetters.sort().join()) {
-    LETTER_BUTTONS.forEach((button) => (button.disabled = true));
+function isWinner(secretWordLetters, guessedCorrectLetters) {
+  if (secretWordLetters.join() === guessedCorrectLetters.sort().join()) {
     return true;
+  } else {
+    return false;
   }
-  return false;
 }
-
-function displayGuessedCorrectLetter(inputLetter) {
-  const LETTER_ELEMENTS = document.querySelectorAll(".secret-letter");
-
-  LETTER_ELEMENTS.forEach((letter) => {
-    if (letter.innerHTML.toLowerCase() === inputLetter) {
-      letter.classList.remove("secret-letter-hidden");
-    }
-  });
-}
-
-const displayWinnerMessage = () => {
-  const WINNER_MESSAGE = document.querySelector(".winner-message");
-  const ALPHABET = document.querySelector(".alphabet");
-  const COUNTDOWN = document.querySelector(".countdown");
-
-  ALPHABET.classList.add("display-none");
-  WINNER_MESSAGE.classList.remove("display-none");
-
-  COUNTDOWN.classList.add("countdown-winner");
-};
-
-const RETRY_BUTTON = document.getElementById("retry-button");
-
-RETRY_BUTTON.addEventListener("click", () => {
-  location.reload();
-});
--- keyBindings.js
+++ keyBindings.js
@@ -1,161 +1,188 @@
+const ELEMENT_LETTER_A = document.getElementById("letter-a");
+const ELEMENT_LETTER_B = document.getElementById("letter-b");
+const ELEMENT_LETTER_C = document.getElementById("letter-c");
+const ELEMENT_LETTER_D = document.getElementById("letter-d");
+const ELEMENT_LETTER_E = document.getElementById("letter-e");
+const ELEMENT_LETTER_F = document.getElementById("letter-f");
+const ELEMENT_LETTER_G = document.getElementById("letter-g");
+const ELEMENT_LETTER_H = document.getElementById("letter-h");
+const ELEMENT_LETTER_I = document.getElementById("letter-i");
+const ELEMENT_LETTER_J = document.getElementById("letter-j");
+const ELEMENT_LETTER_K = document.getElementById("letter-k");
+const ELEMENT_LETTER_L = document.getElementById("letter-l");
+const ELEMENT_LETTER_M = document.getElementById("letter-m");
+const ELEMENT_LETTER_N = document.getElementById("letter-n");
+const ELEMENT_LETTER_O = document.getElementById("letter-o");
+const ELEMENT_LETTER_P = document.getElementById("letter-p");
+const ELEMENT_LETTER_Q = document.getElementById("letter-q");
+const ELEMENT_LETTER_R = document.getElementById("letter-r");
+const ELEMENT_LETTER_S = document.getElementById("letter-s");
+const ELEMENT_LETTER_T = document.getElementById("letter-t");
+const ELEMENT_LETTER_U = document.getElementById("letter-u");
+const ELEMENT_LETTER_V = document.getElementById("letter-v");
+const ELEMENT_LETTER_W = document.getElementById("letter-w");
+const ELEMENT_LETTER_X = document.getElementById("letter-x");
+const ELEMENT_LETTER_Y = document.getElementById("letter-y");
+const ELEMENT_LETTER_Z = document.getElementById("letter-z");
+
 export const listenToKeyboard = () => {
   document.addEventListener("keydown", (event) => {
     switch (event.key) {
       case "a":
-        document.getElementById("letter-a").click();
+        ELEMENT_LETTER_A.click();
         break;
       case "A":
-        document.getElementById("letter-a").click();
+        ELEMENT_LETTER_A.click();
         break;
       case "b":
-        document.getElementById("letter-b").click();
+        ELEMENT_LETTER_B.click();
         break;
       case "B":
-        document.getElementById("letter-b").click();
+        ELEMENT_LETTER_B.click();
         break;
       case "c":
-        document.getElementById("letter-c").click();
+        ELEMENT_LETTER_C.click();
         break;
       case "C":
-        document.getElementById("letter-c").click();
+        ELEMENT_LETTER_C.click();
         break;
       case "d":
-        document.getElementById("letter-d").click();
+        ELEMENT_LETTER_D.click();
         break;
       case "D":
-        document.getElementById("letter-d").click();
+        ELEMENT_LETTER_D.click();
         break;
       case "e":
-        document.getElementById("letter-e").click();
+        ELEMENT_LETTER_E.click();
         break;
       case "E":
-        document.getElementById("letter-e").click();
+        ELEMENT_LETTER_E.click();
         break;
       case "f":
-        document.getElementById("letter-f").click();
+        ELEMENT_LETTER_F.click();
         break;
       case "F":
-        document.getElementById("letter-f").click();
+        ELEMENT_LETTER_F.click();
         break;
       case "g":
-        document.getElementById("letter-g").click();
+        ELEMENT_LETTER_G.click();
         break;
       case "G":
-        document.getElementById("letter-g").click();
+        ELEMENT_LETTER_G.click();
         break;
       case "h":
-        document.getElementById("letter-h").click();
+        ELEMENT_LETTER_H.click();
         break;
       case "H":
-        document.getElementById("letter-h").click();
+        ELEMENT_LETTER_H.click();
         break;
       case "i":
-        document.getElementById("letter-i").click();
+        ELEMENT_LETTER_I.click();
         break;
       case "I":
-        document.getElementById("letter-i").click();
+        ELEMENT_LETTER_I.click();
         break;
       case "j":
-        document.getElementById("letter-j").click();
+        ELEMENT_LETTER_J.click();
         break;
       case "J":
-        document.getElementById("letter-j").click();
+        ELEMENT_LETTER_J.click();
         break;
       case "k":
-        document.getElementById("letter-k").click();
+        ELEMENT_LETTER_K.click();
         break;
       case "K":
-        document.getElementById("letter-k").click();
+        ELEMENT_LETTER_K.click();
         break;
       case "l":
-        document.getElementById("letter-l").click();
+        ELEMENT_LETTER_L.click();
         break;
       case "L":
-        document.getElementById("letter-l").click();
+        ELEMENT_LETTER_L.click();
         break;
       case "m":
-        document.getElementById("letter-m").click();
+        ELEMENT_LETTER_M.click();
         break;
       case "M":
-        document.getElementById("letter-m").click();
+        ELEMENT_LETTER_M.click();
         break;
       case "n":
-        document.getElementById("letter-n").click();
+        ELEMENT_LETTER_N.click();
         break;
       case "N":
-        document.getElementById("letter-n").click();
+        ELEMENT_LETTER_N.click();
         break;
       case "o":
-        document.getElementById("letter-o").click();
+        ELEMENT_LETTER_O.click();
         break;
       case "O":
-        document.getElementById("letter-o").click();
+        ELEMENT_LETTER_O.click();
         break;
       case "p":
-        document.getElementById("letter-p").click();
+        ELEMENT_LETTER_P.click();
         break;
       case "P":
-        document.getElementById("letter-p").click();
+        ELEMENT_LETTER_P.click();
         break;
       case "q":
-        document.getElementById("letter-q").click();
+        ELEMENT_LETTER_Q.click();
         break;
       case "Q":
-        document.getElementById("letter-q").click();
+        ELEMENT_LETTER_Q.click();
         break;
       case "r":
-        document.getElementById("letter-r").click();
+        ELEMENT_LETTER_R.click();
         break;
       case "R":
-        document.getElementById("letter-r").click();
+        ELEMENT_LETTER_R.click();
         break;
       case "s":
-        document.getElementById("letter-s").click();
+        ELEMENT_LETTER_S.click();
         break;
       case "S":
-        document.getElementById("letter-s").click();
+        ELEMENT_LETTER_S.click();
         break;
       case "t":
-        document.getElementById("letter-t").click();
+        ELEMENT_LETTER_T.click();
         break;
       case "T":
-        document.getElementById("letter-t").click();
+        ELEMENT_LETTER_T.click();
         break;
       case "u":
-        document.getElementById("letter-u").click();
+        ELEMENT_LETTER_U.click();
         break;
       case "U":
-        document.getElementById("letter-u").click();
+        ELEMENT_LETTER_U.click();
         break;
       case "v":
-        document.getElementById("letter-v").click();
+        ELEMENT_LETTER_V.click();
         break;
       case "V":
-        document.getElementById("letter-v").click();
+        ELEMENT_LETTER_V.click();
         break;
       case "w":
-        document.getElementById("letter-w").click();
+        ELEMENT_LETTER_W.click();
         break;
       case "W":
-        document.getElementById("letter-w").click();
+        ELEMENT_LETTER_W.click();
         break;
       case "x":
-        document.getElementById("letter-x").click();
+        ELEMENT_LETTER_X.click();
         break;
       case "X":
-        document.getElementById("letter-x").click();
+        ELEMENT_LETTER_X.click();
         break;
       case "y":
-        document.getElementById("letter-y").click();
+        ELEMENT_LETTER_Y.click();
         break;
       case "Y":
-        document.getElementById("letter-y").click();
+        ELEMENT_LETTER_Y.click();
         break;
       case "z":
-        document.getElementById("letter-z").click();
+        ELEMENT_LETTER_Z.click();
         break;
       case "Z":
-        document.getElementById("letter-z").click();
+        ELEMENT_LETTER_Z.click();
         break;
       default:
         break;
